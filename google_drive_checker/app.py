89ebc7c2068b226939dc6f218281af85a059326b
NokASur <asurov2017@litsey2.ru>
2025-05-06 02:09:48 +0300

    better logging, collecting data from the disc and some encoding dingsbums

--- google_drive_checker/app.py
+++ google_drive_checker/app.py
@@ -1,40 +1,29 @@
+import json
 import time
 import datetime
-import logging
-from logging.handlers import RotatingFileHandler
 import os.path
 from pip._internal.network.auth import Credentials
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build, Resource
+
+from logs.logging_module import logger
 from constants import (CHECKER_START_TIME, CHECKER_END_TIME,
                        CHECKER_FREQUENCY_SECONDS, API_SCOPES,
                        TOKEN_FILE, CREDENTIALS_FILE)
-
-all_logs_handler = RotatingFileHandler(
-    filename="google_drive_checker.log",
-    maxBytes=10 * 1024 * 1024,
-    backupCount=5,
-    encoding='utf-8',
-    mode='a',
-
-)
-all_logs_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-all_logs_handler.setLevel(logging.DEBUG)
-
-logger = logging.getLogger(__name__)
-logger.addHandler(all_logs_handler,)
 
 
 def authenticate_drive() -> Credentials:
     """
     Uses token to gain access to Google Drive API if it exists
     Generates a new one using credentials.json if not
+    Returns a Credentials object you can build a service with
     """
 
     logger.info("Starting authentication with Google Drive API")
-    creds = None
+    creds: Credentials | None = None
+
     if os.path.exists(TOKEN_FILE):
         creds = Credentials.from_authorized_user_file(TOKEN_FILE)
     if not creds or not creds.valid:
@@ -45,12 +34,27 @@ def authenticate_drive() -> Credentials:
             creds = flow.run_local_server(port=0)
         with open(TOKEN_FILE, 'w') as token:
             token.write(creds.to_json())
+    return creds
 
-    return build('drive', 'v3', credentials=creds)
 
+def download_new_report(service: Resource) -> str:
+    """
+    :param service: drive service that we download the report from
+    :return: string with the name of the report, or "" if no report was found
+    """
 
-def find_new_report():
-    pass
+    logger.info("Trying to identificate and download a new report")
+    try:
+        value = service.files().list().execute()
+        filtered = (list(filter(lambda d: d['title'] == 'SalaryReports', value['items'])))
+        filtered_data = json.dumps(filtered, sort_keys=True, indent=4)
+        data = json.dumps(value, sort_keys=True, indent=4, ensure_ascii=False)
+        print(data)
+    except Exception as e:
+        logger.error("Problem with downloading new files: " + e.__str__())
+    finally:
+        service.close()
+    return ""
 
 
 def check_drive() -> bool:
@@ -60,16 +64,21 @@ def check_drive() -> bool:
     """
 
     logger.info("Authenticating google Drive...")
-    drive_data: Credentials = authenticate_drive()
 
-    if drive_data:
-        file = find_new_report()
-        if file:
-            return True
+    drive_credentials_data: Credentials = authenticate_drive()
+
+    if drive_credentials_data:
+        drive_service: Resource = build('drive', 'v2', credentials=drive_credentials_data)
+        if drive_service:
+            report_name = download_new_report(drive_service)
+            if report_name:
+                return True
+            else:
+                logger.error(f"No new report found. Recheck in {CHECKER_FREQUENCY_SECONDS} second(s)")
         else:
-            logger.error(f"No new report found. Recheck in {CHECKER_FREQUENCY_SECONDS} second(s)")
+            logger.error("Drive not available")
     else:
-        logger.error("Drive not available")
+        logger.error("Problem creating google drive credentials")
     return False
 
 
@@ -81,7 +90,8 @@ def app() -> None:
         # if CHECKER_START_TIME <= now.time() <= CHECKER_END_TIME: # Commented for debug
         if True:
 
-            if last_execution_time is None or now - last_execution_time >= datetime.timedelta(CHECKER_FREQUENCY_SECONDS):
+            if last_execution_time is None or now - last_execution_time >= datetime.timedelta(
+                    CHECKER_FREQUENCY_SECONDS):
                 last_execution_time = now
                 result = check_drive()
 
